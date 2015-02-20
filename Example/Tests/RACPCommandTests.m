7e74c1d59e6cf870ac7e6f4ccbd238b312d0d0e7
Nathaniel Hamming <nhamming@ehealthinnovation.org>
2015-02-20 10:37:27 -0400

    Updating the tests to use new file names and enum names

--- Example/Tests/RACPCommandTests.m
+++ Example/Tests/RACPCommandTests.m
@@ -6,40 +6,40 @@
 //  Copyright (c) 2015 University Health Network.
 //
 
-#import <UHNBLEController/RecordAccessControlPoint.h>
+#import <UHNBLEController/UHNRecordAccessControlPoint.h>
 
 SpecBegin(RACPCommandSpecs)
 
 describe(@"RACP report stored records command formatting", ^{
     
     it(@"should construct a report all stored records command", ^{
-        NSData *reportAllRecordsCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReport, kRACPOperatorRecordsAll} length:2];
+        NSData *reportAllRecordsCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReport, RACPOperatorRecordsAll} length:2];
         NSData *testCommand = [NSData reportAllStoredRecords];
         expect(testCommand).to.equal(reportAllRecordsCommand);
     });
 
     it(@"should construct a report first stored record command", ^{
-        NSData *reportFirstRecordCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReport, kRACPOperatorRecordFirst} length:2];
+        NSData *reportFirstRecordCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReport, RACPOperatorRecordFirst} length:2];
         NSData *testCommand = [NSData reportFirstStoredRecord];
         expect(testCommand).to.equal(reportFirstRecordCommand);
     });
 
     it(@"should construct a report last stored record command", ^{
-        NSData *reportLastRecordCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReport, kRACPOperatorRecordLast} length:2];
+        NSData *reportLastRecordCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReport, RACPOperatorRecordLast} length:2];
         NSData *testCommand = [NSData reportLastStoredRecord];
         expect(testCommand).to.equal(reportLastRecordCommand);
     });
 
     it(@"should construct a report stored records less than or equal to command", ^{
         uint16_t maxTimeOffset = 15;
-        NSData *reportRecordsLessThanCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReport, kRACPOperatorLessThanEqualTo, kRACPFilterTypeTimeOffset, maxTimeOffset, maxTimeOffset >> 8} length:5];
+        NSData *reportRecordsLessThanCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReport, RACPOperatorLessThanEqualTo, RACPFilterTypeTimeOffset, maxTimeOffset, maxTimeOffset >> 8} length:5];
         NSData *testCommand = [NSData reportStoredRecordsLessThanOrEqualToTimeOffset: 15];
         expect(testCommand).to.equal(reportRecordsLessThanCommand);
     });
     
     it(@"should construct a report stored records greater than or equal to command", ^{
         uint16_t minTimeOffset = 30;
-        NSData *reportRecordsGreaterThanCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReport, kRACPOperatorGreaterThanEqualTo, kRACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8} length:5];
+        NSData *reportRecordsGreaterThanCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReport, RACPOperatorGreaterThanEqualTo, RACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8} length:5];
         NSData *testCommand = [NSData reportStoredRecordsGreaterThanOrEqualToTimeOffset: 30];
         expect(testCommand).to.equal(reportRecordsGreaterThanCommand);
     });
@@ -47,7 +47,7 @@ describe(@"RACP report stored records command formatting", ^{
     it(@"should construct a report stored records between command", ^{
         uint16_t minTimeOffset = 30;
         uint16_t maxTimeOffset = 15;
-        NSData *reportRecordsBetweenCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReport, kRACPOperatorWithinRange, kRACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8, maxTimeOffset, maxTimeOffset >> 8} length:7];
+        NSData *reportRecordsBetweenCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReport, RACPOperatorWithinRange, RACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8, maxTimeOffset, maxTimeOffset >> 8} length:7];
         NSData *testCommand = [NSData reportStoredRecordsBetween:minTimeOffset and:maxTimeOffset];
         expect(testCommand).to.equal(reportRecordsBetweenCommand);
     });
@@ -57,33 +57,33 @@ describe(@"RACP report stored records command formatting", ^{
 describe(@"RACP delete stored records command formatting", ^{
     
     it(@"should construct a delete all stored records command", ^{
-        NSData *deleteAllRecordsCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsDelete, kRACPOperatorRecordsAll} length:2];
+        NSData *deleteAllRecordsCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsDelete, RACPOperatorRecordsAll} length:2];
         NSData *testCommand = [NSData deleteAllStoredRecords];
         expect(testCommand).to.equal(deleteAllRecordsCommand);
     });
     
     it(@"should construct a delete first stored record command", ^{
-        NSData *deleteFirstRecordCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsDelete, kRACPOperatorRecordFirst} length:2];
+        NSData *deleteFirstRecordCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsDelete, RACPOperatorRecordFirst} length:2];
         NSData *testCommand = [NSData deleteFirstStoredRecord];
         expect(testCommand).to.equal(deleteFirstRecordCommand);
     });
     
     it(@"should construct a delete last stored record command", ^{
-        NSData *deleteLastRecordCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsDelete, kRACPOperatorRecordLast} length:2];
+        NSData *deleteLastRecordCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsDelete, RACPOperatorRecordLast} length:2];
         NSData *testCommand = [NSData deleteLastStoredRecord];
         expect(testCommand).to.equal(deleteLastRecordCommand);
     });
     
     it(@"should construct a delete stored records less than or equal to command", ^{
         uint16_t maxTimeOffset = 15;
-        NSData *deleteRecordsLessThanCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsDelete, kRACPOperatorLessThanEqualTo, kRACPFilterTypeTimeOffset, maxTimeOffset, maxTimeOffset >> 8} length:5];
+        NSData *deleteRecordsLessThanCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsDelete, RACPOperatorLessThanEqualTo, RACPFilterTypeTimeOffset, maxTimeOffset, maxTimeOffset >> 8} length:5];
         NSData *testCommand = [NSData deleteStoredRecordsLessThanOrEqualToTimeOffset: 15];
         expect(testCommand).to.equal(deleteRecordsLessThanCommand);
     });
     
     it(@"should construct a delete stored records greater than or equal to command", ^{
         uint16_t minTimeOffset = 30;
-        NSData *deleteRecordsGreaterThanCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsDelete, kRACPOperatorGreaterThanEqualTo, kRACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8} length:5];
+        NSData *deleteRecordsGreaterThanCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsDelete, RACPOperatorGreaterThanEqualTo, RACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8} length:5];
         NSData *testCommand = [NSData deleteStoredRecordsGreaterThanOrEqualToTimeOffset: 30];
         expect(testCommand).to.equal(deleteRecordsGreaterThanCommand);
     });
@@ -91,7 +91,7 @@ describe(@"RACP delete stored records command formatting", ^{
     it(@"should construct a delete stored records between command", ^{
         uint16_t minTimeOffset = 30;
         uint16_t maxTimeOffset = 15;
-        NSData *deleteRecordsBetweenCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsDelete, kRACPOperatorWithinRange, kRACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8, maxTimeOffset, maxTimeOffset >> 8} length:7];
+        NSData *deleteRecordsBetweenCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsDelete, RACPOperatorWithinRange, RACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8, maxTimeOffset, maxTimeOffset >> 8} length:7];
         NSData *testCommand = [NSData deleteStoredRecordsBetween:minTimeOffset and:maxTimeOffset];
         expect(testCommand).to.equal(deleteRecordsBetweenCommand);
     });
@@ -101,33 +101,33 @@ describe(@"RACP delete stored records command formatting", ^{
 describe(@"RACP report number of stored records command formatting", ^{
     
     it(@"should construct a report number of all stored records command", ^{
-        NSData *reportNumberOfAllRecordsCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReportNumber, kRACPOperatorRecordsAll} length:2];
+        NSData *reportNumberOfAllRecordsCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReportNumber, RACPOperatorRecordsAll} length:2];
         NSData *testCommand = [NSData reportNumberOfAllStoredRecords];
         expect(testCommand).to.equal(reportNumberOfAllRecordsCommand);
     });
     
     it(@"should construct a report number of first stored record command", ^{
-        NSData *reportNumberOfFirstRecordCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReportNumber, kRACPOperatorRecordFirst} length:2];
+        NSData *reportNumberOfFirstRecordCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReportNumber, RACPOperatorRecordFirst} length:2];
         NSData *testCommand = [NSData reportNumberOfFirstStoredRecord];
         expect(testCommand).to.equal(reportNumberOfFirstRecordCommand);
     });
     
     it(@"should construct a delete last stored record command", ^{
-        NSData *reportNumberOfLastRecordCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReportNumber, kRACPOperatorRecordLast} length:2];
+        NSData *reportNumberOfLastRecordCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReportNumber, RACPOperatorRecordLast} length:2];
         NSData *testCommand = [NSData reportNumberOfLastStoredRecord];
         expect(testCommand).to.equal(reportNumberOfLastRecordCommand);
     });
     
     it(@"should construct a delete stored records less than or equal to command", ^{
         uint16_t maxTimeOffset = 15;
-        NSData *reportNumberOfRecordsLessThanCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReportNumber, kRACPOperatorLessThanEqualTo, kRACPFilterTypeTimeOffset, maxTimeOffset, maxTimeOffset >> 8} length:5];
+        NSData *reportNumberOfRecordsLessThanCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReportNumber, RACPOperatorLessThanEqualTo, RACPFilterTypeTimeOffset, maxTimeOffset, maxTimeOffset >> 8} length:5];
         NSData *testCommand = [NSData reportNumberOfStoredRecordsLessThanOrEqualToTimeOffset: 15];
         expect(testCommand).to.equal(reportNumberOfRecordsLessThanCommand);
     });
     
     it(@"should construct a delete stored records greater than or equal to command", ^{
         uint16_t minTimeOffset = 30;
-        NSData *reportNumberOfRecordsGreaterThanCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReportNumber, kRACPOperatorGreaterThanEqualTo, kRACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8} length:5];
+        NSData *reportNumberOfRecordsGreaterThanCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReportNumber, RACPOperatorGreaterThanEqualTo, RACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8} length:5];
         NSData *testCommand = [NSData reportNumberOfStoredRecordsGreaterThanOrEqualToTimeOffset: 30];
         expect(testCommand).to.equal(reportNumberOfRecordsGreaterThanCommand);
     });
@@ -135,7 +135,7 @@ describe(@"RACP report number of stored records command formatting", ^{
     it(@"should construct a delete stored records between command", ^{
         uint16_t minTimeOffset = 30;
         uint16_t maxTimeOffset = 15;
-        NSData *reportNumberOfRecordsBetweenCommand = [NSData dataWithBytes:(char[]){kRACPOpCodeStoredRecordsReportNumber, kRACPOperatorWithinRange, kRACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8, maxTimeOffset, maxTimeOffset >> 8} length:7];
+        NSData *reportNumberOfRecordsBetweenCommand = [NSData dataWithBytes:(char[]){RACPOpCodeStoredRecordsReportNumber, RACPOperatorWithinRange, RACPFilterTypeTimeOffset, minTimeOffset, minTimeOffset >> 8, maxTimeOffset, maxTimeOffset >> 8} length:7];
         NSData *testCommand = [NSData reportNumberOfStoredRecordsBetween:minTimeOffset and:maxTimeOffset];
         expect(testCommand).to.equal(reportNumberOfRecordsBetweenCommand);
     });
@@ -145,7 +145,7 @@ describe(@"RACP report number of stored records command formatting", ^{
 describe(@"RACP abort command formatting", ^{
     
     it(@"should construct an abort command", ^{
-        NSData *abortCommnd = [NSData dataWithBytes:(char[]){kRACPOpCodeAbortOperation, kRACPOperatorNull} length:2];
+        NSData *abortCommnd = [NSData dataWithBytes:(char[]){RACPOpCodeAbortOperation, RACPOperatorNull} length:2];
         NSData *testCommand = [NSData abortOperation];
         expect(testCommand).to.equal(abortCommnd);
     });
--- Example/Tests/RACPParserTests.m
+++ Example/Tests/RACPParserTests.m
@@ -6,128 +6,128 @@
 //  Copyright (c) 2015 University Health Network.
 //
 
-#import <UHNBLEController/RecordAccessControlPoint.h>
+#import <UHNBLEController/UHNRecordAccessControlPoint.h>
 
 SpecBegin(RACPParserSpecs)
 
 describe(@"RACP response parsing", ^{
     
     it(@"should be able to parse a successful report records response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsReport, kRACPSuccess} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsReport, RACPSuccess} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beTruthy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsReport);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPSuccess);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsReport);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPSuccess);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse a not supported op code response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsDelete, kRACPNotSupportedOpCode} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsDelete, RACPNotSupportedOpCode} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsDelete);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPNotSupportedOpCode);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsDelete);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPNotSupportedOpCode);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse a not supported operator response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsDelete, kRACPNotSupportedOperator} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsDelete, RACPNotSupportedOperator} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsDelete);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPNotSupportedOperator);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsDelete);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPNotSupportedOperator);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse a not supported operand response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsDelete, kRACPNotSupportedOperand} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsDelete, RACPNotSupportedOperand} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsDelete);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPNotSupportedOperand);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsDelete);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPNotSupportedOperand);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse a invalid operator response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsDelete, kRACPInvalidOperator} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsDelete, RACPInvalidOperator} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsDelete);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPInvalidOperator);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsDelete);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPInvalidOperator);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse a invalid operand response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsDelete, kRACPInvalidOperand} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsDelete, RACPInvalidOperand} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsDelete);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPInvalidOperand);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsDelete);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPInvalidOperand);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse a no records found response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsReportNumber, kRACPNoRecordsFound} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsReportNumber, RACPNoRecordsFound} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsReportNumber);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPNoRecordsFound);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsReportNumber);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPNoRecordsFound);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse an abort unsuccessful response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeAbortOperation, kRACPAbortUnsuccessful} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeAbortOperation, RACPAbortUnsuccessful} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeAbortOperation);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPAbortUnsuccessful);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeAbortOperation);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPAbortUnsuccessful);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse an abort unsuccessful response", ^{
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponse, kRACPOperatorNull, kRACPOpCodeStoredRecordsReportNumber, kRACPProcedureNotCompleted} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponse, RACPOperatorNull, RACPOpCodeStoredRecordsReportNumber, RACPProcedureNotCompleted} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beTruthy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beFalsy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponse);
-        expect([racpResponseDetails requestOpCode]).to.equal(kRACPOpCodeStoredRecordsReportNumber);
-        expect([racpResponseDetails responseCodeValue]).to.equal(kRACPProcedureNotCompleted);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponse);
+        expect([racpResponseDetails requestOpCode]).to.equal(RACPOpCodeStoredRecordsReportNumber);
+        expect([racpResponseDetails responseCodeValue]).to.equal(RACPProcedureNotCompleted);
         expect([racpResponseDetails numberOfRecords]).to.beNil;
     });
     
     it(@"should be able to parse an number of stored records response", ^{
         uint16_t numOfRecords = 10;
-        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){kRACPOpCodeResponseStoredRecordsReportNumber, kRACPOperatorNull, numOfRecords, numOfRecords >> 8} length:4];
+        NSData *successfulReportRecordReponse = [NSData dataWithBytes:(char[]){RACPOpCodeResponseStoredRecordsReportNumber, RACPOperatorNull, numOfRecords, numOfRecords >> 8} length:4];
         NSDictionary *racpResponseDetails = [successfulReportRecordReponse parseRACPResponse];
         expect([racpResponseDetails isGeneralResponse]).to.beFalsy;
         expect([racpResponseDetails isNumberOfRecordResponse]).to.beTruthy;
         expect([racpResponseDetails isSuccessfulResponseReportRecords]).to.beFalsy;
-        expect([racpResponseDetails responseOpCode]).to.equal(kRACPOpCodeResponseStoredRecordsReportNumber);
+        expect([racpResponseDetails responseOpCode]).to.equal(RACPOpCodeResponseStoredRecordsReportNumber);
         expect([racpResponseDetails requestOpCode]).to.equal(255); // no request op code found
         expect([racpResponseDetails responseCodeValue]).to.equal(255); // no response code value found
         expect([racpResponseDetails numberOfRecords]).to.equal(10);
